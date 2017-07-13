cf2a9f1837fe46dcc51ef3c67368852376eb1dd3
Robert Bunch <rob@digitalcrafts.com>
2017-07-13 15:32:58 -0400

    insert in registration

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,11 @@ var mysql = require('mysql');
 // Include config file. Go up from routes, down into config, config.js
 var config = require('../config/config');
 
+// include bcrpyt for hasing and checking password
+var bcrypt = require('bcrypt-nodejs');
+// include rand-token for generating user token
+var randToken = require('rand-token')
+
 // set up the connection with options
 var connection = mysql.createConnection({
 	host: config.host,
@@ -31,26 +36,51 @@ router.get('/productlines/get', (req, res)=>{
 });
 
 router.post('/register', (req, res)=>{
+	console.log(req.body)
+
 	const name = req.body.name;
 	const email = req.body.email;
 	const accountType = 'customer';
-	const password = req.body.password;
-	console.log(req.body.city)
+	const password = bcrypt.hashSync(req.body.password);
 	const city = req.body.city;
 	const state = req.body.state;
 	const salesRep = req.body.salesRep
-	var insertQuery = "INSERT INTO users (type,password) VALUES (?,?)";
-	connection.query(insertQuery,[accountType,password],(error,results)=>{
-		if(error){
-			res.json({
-				msg: error
-			})
-		}else{
-			res.json({
-				msg: "userInserted"
-			})
-		}
-	});
+	const creditLimit = 16000000
+
+	// We want to insert the user into 2 tables: Customers and Users.
+	// Users needs the customerNumber from the Customers table.
+	// Therefore, we need to insert the user into Customers first...
+	// get the ID created by that insert, THEN insert the user into Users.
+
+	// Customers insert query
+	var insertIntoCust = "INSERT INTO customers (customerName, city, state, salesRepEmployeeNumber,creditLimit) VALUES (?,?,?,?,?)"
+	// Run the query (for now autoset the sales rep to 1337)
+	connection.query(insertIntoCust,[name,city,state,1337,creditLimit],(error, results)=>{
+		// Get the ID that was used in the customers insert
+		const newID = results.insertId
+		// Get the current timestamp
+		var currTimeStamp = Date.now() / 1000;
+		// Set up a token for this user. We will give this back to React
+		var token = randToken.uid(40);
+		// Users insert query
+		var insertQuery = "INSERT INTO users (uid,type,password,created,token) VALUES (?,?,?,?,?)";
+		// Run the query. Use error2 and results2 because are already used results and error
+		
+		connection.query(insertQuery,[newID, accountType,password, currTimeStamp, token],(error2,results2)=>{
+			if(error2){
+				res.json({
+					msg: error2
+				})
+			}else{
+				res.json({
+					msg: "userInserted",
+					token: token
+				})
+			}
+		});
+	})
+
+
 })	
 
 module.exports = router;
