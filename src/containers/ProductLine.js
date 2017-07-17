026494fa4b65e3334f782b4290c0d338d7f3fd24
Robert Bunch <rob@digitalcrafts.com>
2017-07-17 11:51:17 -0400

    add product table

--- src/containers/ProductLine.js
+++ src/containers/ProductLine.js
@@ -16,12 +16,54 @@ class ProductLine extends Component{
 		const url = window.hostAddress + `/productlines/${pl}/get`
 		$.getJSON(url,(data)=>{
 			console.log(data);
+			this.setState({
+				productList: data
+			})
 		});
 	}
 
 	render(){
+
+		var productTableArray = [];
+		this.state.productList.map((product, index)=>{
+			if(product.quantityInStock > 100){
+				var inStock = "In Stock!"
+			}else if(product.quantityInStock >0){
+				var inStock = '<td className="text-caution">Order Soon!</td>'
+			}else{
+				var inStock = '<td className="text-danger">Out of stock!</td>'
+			}
+			productTableArray.push(
+				<tr key={index} >
+					<td>{product.productName}</td>
+					<td>{product.productScale}</td>
+					<td>{product.productVendor}</td>
+					<td>{product.productDescription}</td>
+					{inStock}
+					<td>{product.buyPrice}</td>
+					<td>{product.MSRP}</td>
+				</tr>
+			)
+		})
+
 		return(
-			<h1>Product List Page</h1>
+			<div>
+				<h1>{this.props.match.params.productLine}</h1>
+				<table className="table table-striped">
+					<thead>
+						<th>Product Name</th>
+						<th>Model Scale</th>
+						<th>Made By</th>
+						<th>Description</th>
+						<th>In Stock</th>
+						<th>Your Price!</th>
+						<th>MSRP</th>
+					</thead>
+					<tbody>
+						{productTableArray}
+					</tbody>
+				</table>
+			</div>
 		)		
 	}
 }
