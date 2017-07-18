d75610a762646143f6f39c1484090f5b352d69e6
Robert Bunch <rob@digitalcrafts.com>
2017-07-18 15:37:16 -0400

    add button

--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux'
 import GetCart from '../actions/GetCart'
+import ProductTableRow from '../components/ProductTableRow';
+import $ from 'jquery'
 
 class Cart extends Component{
 
@@ -13,10 +15,61 @@ class Cart extends Component{
 		}
 	}
 
+	makePayment() {
+        var handler = window.StripeCheckout.configure({
+            key: 'pk_test_At6FBgV0uygrKlJ76ivhxgDv',
+            locale: 'auto',
+            token: (token) => {
+                var theData = {
+                    amount: 10 * 100,
+                    stripeToken: token.id,
+                    userToken: this.props.tokenData,
+                }
+                $.ajax({
+                    method: 'POST',
+                    url: window.hostAddress+'/stripe',
+                    data: theData
+                }).done((data) => {
+                    console.log(data);
+                    if (data.msg === 'paymentSuccess') {
+
+                    }
+                });
+            }
+        });
+        handler.open({
+            name: "Pay Now",
+            description: 'Pay Now',
+            amount: 10 * 100
+        })
+    }
+
 	render(){
+		var cartArray = [];
+		this.props.cartInfo.products.map((product,index)=>{
+			console.log(product)
+			cartArray.push(
+				<ProductTableRow 
+					key={index} 
+					product={product} 
+					addToCart={null}
+					loggedIn={false}
+					token={null}
+				/>				
+			)
+		})
+
+
 		console.log(this.props.cartInfo)
 		return(
-			<h1>Cart</h1>
+			<div>
+				<div>
+					<button className="btn btn-primary" onClick={this.makePayment}>
+						Pay now!
+					</button>
+				</div>
+				{cartArray}
+			</div>
 		)
 	}
 }
